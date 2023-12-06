b362a4855c7683354f8cec1a0e02bb9b6f1237b5
Hong-Rui-Peter <hongrui8523@gmail.com>
2023-12-06 16:59:32 +0800

    First Python File Commit

---
+++ PyGame/[00]相關補充教材/自動執行檔製作/myhead.py
@@ -0,0 +1,6 @@
+'''
+取表頭示範程式
+'''
+import MYMODULE as mm
+myHead=mm._myHead()
+print(myHead)
---
+++ PyGame/[00]相關補充教材/自動執行檔製作/saying.py
@@ -0,0 +1,18 @@
+'''
+自訂模組的示範程式
+'''
+
+import MYMODULE as mm
+
+while True:
+    mm._show(mm._today())
+    mm._show(mm._now(),0.1)
+    mm._show(mm._saying(),0.5)
+    anS=input("沒錯吧.....")
+    if anS=="0":
+        break
+    
+    
+    
+    
+    
