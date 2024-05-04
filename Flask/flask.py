8f37a3a7e774f4d415c379db371f3986196e207d
Hong-Rui-Peter <hongrui8523@gmail.com>
2024-05-04 21:19:46 +0800

    add flask

---
+++ Flask/flask.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template,request
+app = Flask(__name__)
+
+
+@app.route("/")
+def default_page():
+    return "<html><body>Hello World</body></html>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)
